af3fcff19d1f2780d754af08c98a27dd7232b848
Rodolfo Silva <35670736+Th0l@users.noreply.github.com>
2020-12-06 16:04:40 +0000

    Add files via upload

--- MainApp.java
+++ MainApp.java
@@ -1,9 +1,12 @@
 import Agents.AgenteSystem;
 import Interface.GUI_mapSize;
 import JadePlatform.MainContainer;
+import Util.Coordenadas;
 import Util.Mapa;
 
 import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainApp {
 
@@ -12,8 +15,30 @@ public class MainApp {
 
     public static void agentStartUp(Mapa mapa){
         MainContainer mc = new MainContainer();
+
         mc.startInterface(mapa);
-        //mc.startSystemAgent(mapa.getTamanho(), mapa.getEstacoes());
+
+        /*
+            Começar a criação de estações
+            O primeiro ciclo for serve para não o estar a fazer todas as vezes que se cria uma estação nova...
+         */
+        int numeroDeEstacoes = (int) Math.pow(mapa.getEstacoes(),2);
+        Map<String,Double> ocupacaoEstacoes = new HashMap<>();
+        for(int i=0;i<numeroDeEstacoes;i++){
+            String nome = "Estacao "+(i+1);
+            ocupacaoEstacoes.put(nome,(double)50); // No inicio do sistema, todas as estações tem 50% das sua capacidade maxima
+                                                   // Pode-se mudar depois se 50 for demasiado baixo...
+        }
+
+        for(int i=0;i<numeroDeEstacoes;i++){ // Dar startUp a todas as estações
+            String nome = "Estacao "+(i+1);
+            mc.startEstacao((i+1),mapa,ocupacaoEstacoes);
+        }
+
+        try{ Thread.sleep(1000); } //Sleep para dar tempo as estaçôes de se inscreverem nas Paginas Amarelas
+        catch(Exception e){ e.printStackTrace(); }
+
+        //mc.startSystemAgent(mapa);
 
     }
 
